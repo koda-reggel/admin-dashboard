1bb85726bf3a7ae563b3772e3ab37950b88f138e
koda-reggel <reggel.campanilla@kodakollectiv.com>
2024-10-18 18:01:20 +0800

    Feat;added sidebar

--- app/_sections/sidebar.tsx
+++ app/_sections/sidebar.tsx
@@ -1,17 +1,23 @@
 "use client";
 
+import { SiWindows10 } from "react-icons/si";
+import { FaRegUser } from "react-icons/fa";
+
 export default function Sidebar() {
   return (
-    <div className="flex flex-col w-52 bg-green-900">
-      <div className="flex justify-center">Siohioma</div>
-      <label>MENU</label>
-      <label>MENU</label>
-      <label>MENU</label>
-      <label>MENU</label>
-      <label>MENU</label>
-      <label>MENU</label>
-      <label>MENU</label>
-      <label>MENU</label>
+    <div className="flex flex-col  w-52 bg-green-900 h-screen">
+      <div className="flex justify-center p-2 text-white text-xl">Siohioma</div>
+      <div className="flex text-white bg-green-500 p-4">Menu</div>
+      <div className=" text-white flex gap-8 flex-col p-4">
+        <div className="flex gap-4 items-center">
+          <SiWindows10 />
+          <span>Overview</span>
+        </div>
+        <div className="flex gap-4 items-center">
+          <FaRegUser />
+          <span>User</span>
+        </div>
+      </div>
     </div>
   );
 }
