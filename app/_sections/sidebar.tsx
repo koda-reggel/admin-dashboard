b2c6623eee4c4afc682e59eb9e55b63a555a9638
koda-reggel <reggel.campanilla@kodakollectiv.com>
2024-10-18 17:26:46 +0800

    second commit

---
+++ app/_sections/sidebar.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function Sidebar() {
+  return (
+    <div className="flex flex-col w-52 bg-green-900">
+      <div className="flex justify-center">Siohioma</div>
+      <label>MENU</label>
+      <label>MENU</label>
+      <label>MENU</label>
+      <label>MENU</label>
+      <label>MENU</label>
+      <label>MENU</label>
+      <label>MENU</label>
+      <label>MENU</label>
+    </div>
+  );
+}
---
+++ app/users/_sections/body.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Body() {
+  return <div className="flex flex-col"></div>;
+}
