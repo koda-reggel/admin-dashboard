67f5b87304e6eeeee9d363ad623ca47affa9abfa
koda-reggel <reggel.campanilla@kodakollectiv.com>
2024-10-18 18:30:49 +0800

    Feat:added user

--- app/users/_sections/body.tsx
+++ app/users/_sections/body.tsx
@@ -1,5 +1,24 @@
 "use client";
 
+import { Input } from "@nextui-org/input";
+
 export default function Body() {
-  return <div className="flex flex-col"></div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex w-full justify-between  bg-red-400 ">
+        <p className="flex items-center">User</p>
+        <div className="flex gap-4">
+          <Input placeholder="Filter users..."></Input>
+          <Input placeholder="Filter users..."></Input>
+        </div>
+      </div>
+      <div className="flex justify-center  gap-4">
+        <Input placeholder="Filter users..."></Input>
+        <Input placeholder="Filter users..."></Input>
+        <div>
+          <Input placeholder="Filter users..."></Input>
+        </div>
+      </div>
+    </div>
+  );
 }
