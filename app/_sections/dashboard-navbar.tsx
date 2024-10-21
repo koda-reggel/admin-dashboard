dadf722e87b82fb6caa4c750e77cf7dc3e741a82
koda-mhel <mhel.tagpuno@kodakollectiv.com>
2024-10-21 15:02:48 +0800

    remove typo

--- app/_sections/dashboard-navbar.tsx
+++ app/_sections/dashboard-navbar.tsx
@@ -6,9 +6,6 @@ import { FiInbox, FiSearch } from "react-icons/fi";
 import { RiBubbleChartLine } from "react-icons/ri";
 
 export default function DashboardNavbar() {
-<<<<<<< HEAD
-  return <div>aaaa</div>;
-=======
   return (
     <div className="flex justify-between items-center max-w-screen-xl mx-auto">
       <div className="flex justify-between items-center w-[865px]">
@@ -31,5 +28,4 @@ export default function DashboardNavbar() {
       </div>
     </div>
   );
->>>>>>> 5e62a6f86b9c8b97bf24866fc9dfe2c15f6772d1
 }
