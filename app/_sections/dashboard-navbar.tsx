634d331d05aa139bf408868b266dd993fc45df36
koda-mhel <mhel.tagpuno@kodakollectiv.com>
2024-10-23 14:55:34 +0800

    few changes

--- app/_sections/dashboard-navbar.tsx
+++ app/_sections/dashboard-navbar.tsx
@@ -21,31 +21,6 @@ export default function DashboardNavbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    // <div className="flex justify-between items-center  w-full lg:max-w-screen-lg xl:max-w-screen-xl">
-    //   <label className="font-semibold text-xl hidden md:block">
-    //     Sales Admin
-    //   </label>
-    //   <label className="font-semibold text-xl md:hidden">Siohioma</label>
-    //   <div className="flex justify-between xl:gap-24 gap-10">
-    //     <div>
-    //       <Input
-    //         placeholder="Search anything in Siohioma.."
-    //         endContent={<FiSearch />}
-    //       />
-    //     </div>
-    //     <div className="lg:flex gap-4 flex-1 justify-end hidden">
-    //       <Button isIconOnly size="sm">
-    //         <FiInbox />
-    //       </Button>
-    //       <Button isIconOnly size="sm">
-    //         <RiBubbleChartLine />
-    //       </Button>
-    //       <Button endContent={<BsFillPlusCircleFill />} size="sm">
-    //         Add new product
-    //       </Button>
-    //     </div>
-    //   </div>
-    // </div>
     <Navbar
       onMenuOpenChange={setIsMenuOpen}
       maxWidth="xl"
