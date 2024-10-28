78e5f1a962ca399f15cad7be7c22222bd8f8e19f
koda-reggel <reggel.campanilla@kodakollectiv.com>
2024-10-28 13:52:09 +0800

    modified for respnsive

--- app/_sections/dashboard-navbar.tsx
+++ app/_sections/dashboard-navbar.tsx
@@ -1,6 +1,18 @@
 "use client";
 
-import { Button, Input } from "@nextui-org/react";
+import {
+  Navbar,
+  NavbarContent,
+  NavbarMenuToggle,
+  NavbarBrand,
+  NavbarItem,
+  Button,
+  NavbarMenu,
+  NavbarMenuItem,
+  Input,
+} from "@nextui-org/react";
+import Link from "next/link";
+import { useState } from "react";
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import { FiInbox, FiSearch } from "react-icons/fi";
 import { RiBubbleChartLine } from "react-icons/ri";
@@ -8,7 +20,6 @@ import { RiBubbleChartLine } from "react-icons/ri";
 export default function DashboardNavbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    
     <Navbar
       onMenuOpenChange={setIsMenuOpen}
       maxWidth="xl"
@@ -29,17 +40,39 @@ export default function DashboardNavbar() {
             placeholder="Search anything in Siohioma.."
             endContent={<FiSearch />}
           />
-        </div>
-      </div>
-      <div className="flex gap-4 flex-1 justify-end">
-        <Button isIconOnly>
-          <FiInbox />
-        </Button>
-        <Button isIconOnly>
-          <RiBubbleChartLine />
-        </Button>
-        <Button endContent={<BsFillPlusCircleFill />}>Add new product</Button>
-      </div>
-    </div>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end" className="hidden md:flex">
+        <NavbarItem>
+          <Button isIconOnly size="sm">
+            <FiInbox />
+          </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <Button isIconOnly size="sm">
+            <RiBubbleChartLine />
+          </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <Button endContent={<BsFillPlusCircleFill />} size="sm">
+            Add new product
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu>
+        <NavbarMenuItem>
+          <Link href="/" className="w-full" size="lg" color="foreground">
+            Dashboard
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <Link href="/users" className="w-full" size="lg" color="foreground">
+            User
+          </Link>
+        </NavbarMenuItem>
+      </NavbarMenu>
+    </Navbar>
   );
 }
