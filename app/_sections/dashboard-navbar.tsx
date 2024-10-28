a06b4241557fbadca4547ac76ed09da4aceff7f1
koda-reggel <reggel.campanilla@kodakollectiv.com>
2024-10-28 13:35:26 +0800

    added video

--- app/_sections/dashboard-navbar.tsx
+++ app/_sections/dashboard-navbar.tsx
@@ -1,104 +1,31 @@
 "use client";
 
-import {
-  Button,
-  Input,
-  Link,
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenu,
-  NavbarMenuItem,
-  NavbarMenuToggle,
-} from "@nextui-org/react";
-import { useState } from "react";
+import { Button, Input } from "@nextui-org/react";
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import { FiInbox, FiSearch } from "react-icons/fi";
 import { RiBubbleChartLine } from "react-icons/ri";
 
 export default function DashboardNavbar() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
   return (
-    // <div className="flex justify-between items-center  w-full lg:max-w-screen-lg xl:max-w-screen-xl">
-    //   <label className="font-semibold text-xl hidden md:block">
-    //     Sales Admin
-    //   </label>
-    //   <label className="font-semibold text-xl md:hidden">Siohioma</label>
-    //   <div className="flex justify-between xl:gap-24 gap-10">
-    //     <div>
-    //       <Input
-    //         placeholder="Search anything in Siohioma.."
-    //         endContent={<FiSearch />}
-    //       />
-    //     </div>
-    //     <div className="lg:flex gap-4 flex-1 justify-end hidden">
-    //       <Button isIconOnly size="sm">
-    //         <FiInbox />
-    //       </Button>
-    //       <Button isIconOnly size="sm">
-    //         <RiBubbleChartLine />
-    //       </Button>
-    //       <Button endContent={<BsFillPlusCircleFill />} size="sm">
-    //         Add new product
-    //       </Button>
-    //     </div>
-    //   </div>
-    // </div>
-    <Navbar
-      onMenuOpenChange={setIsMenuOpen}
-      maxWidth="xl"
-      classNames={{ wrapper: "px-4 h-16" }}
-      className="h-16"
-    >
-      <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className=" lg:hidden"
-        />
-        <NavbarBrand className="font-bold text-xl">Sales Admin</NavbarBrand>
-      </NavbarContent>
-
-      <NavbarContent justify="end">
-        <NavbarItem className="w-full">
+    <div className="flex justify-between items-center max-w-screen-xl mx-auto">
+      <div className="flex justify-between items-center w-[865px]">
+        <label className="font-semibold text-xl">Sales Admin</label>
+        <div>
           <Input
             placeholder="Search anything in Siohioma.."
             endContent={<FiSearch />}
           />
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent justify="end" className="hidden md:flex">
-        <NavbarItem>
-          <Button isIconOnly size="sm">
-            <FiInbox />
-          </Button>
-        </NavbarItem>
-        <NavbarItem>
-          <Button isIconOnly size="sm">
-            <RiBubbleChartLine />
-          </Button>
-        </NavbarItem>
-        <NavbarItem>
-          <Button endContent={<BsFillPlusCircleFill />} size="sm">
-            Add new product
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarMenu>
-        <NavbarMenuItem>
-          <Link href="/" className="w-full" size="lg" color="foreground">
-            Dashboard
-          </Link>
-        </NavbarMenuItem>
-        <NavbarMenuItem>
-          <Link href="/users" className="w-full" size="lg" color="foreground">
-            User
-          </Link>
-        </NavbarMenuItem>
-      </NavbarMenu>
-    </Navbar>
+        </div>
+      </div>
+      <div className="flex gap-4 flex-1 justify-end">
+        <Button isIconOnly>
+          <FiInbox />
+        </Button>
+        <Button isIconOnly>
+          <RiBubbleChartLine />
+        </Button>
+        <Button endContent={<BsFillPlusCircleFill />}>Add new product</Button>
+      </div>
+    </div>
   );
 }
--- app/videos/_sections/body.tsx
+++ app/videos/_sections/body.tsx
@@ -10,22 +10,42 @@ export default function Body() {
     {
       title: "Spider-man",
       img: "https://miro.medium.com/v2/resize:fit:1400/1*iQhzIW0ZffqWaTI10ywLsA.jpeg",
+      duration: "2h 1m",
+      date: "11-11-24",
+      description:
+        "After being bitten by a genetically-modified spider, a shy teenager gains spider-like abilities that he uses to fight injustice as a masked superhero and face a vengeful enemy",
     },
     {
       title: "Joker",
+      duration: "2h 1m",
+      date: "11-11-24",
+      description:
+        "After being bitten by a genetically-modified spider, a shy teenager gains spider-like abilities that he uses to fight injustice as a masked superhero and face a vengeful enemy",
       img: "https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1500x1500/products/89058/93685/Joker-2019-Final-Style-steps-Poster-buy-original-movie-posters-at-starstills__62518.1669120603.jpg?c=2",
     },
     {
       title: "Avengers",
       img: "https://i.ebayimg.com/images/g/36kAAOSw67Zcnatq/s-l1200.jpg",
+      duration: "2h 1m",
+      date: "11-11-24",
+      description:
+        "After being bitten by a genetically-modified spider, a shy teenager gains spider-like abilities that he uses to fight injustice as a masked superhero and face a vengeful enemy",
     },
     {
       title: "Tenet",
       img: "https://i.ebayimg.com/images/g/gEoAAOSw84BfOWDp/s-l1200.jpg",
+      duration: "2h 1m",
+      date: "11-11-24",
+      description:
+        "After being bitten by a genetically-modified spider, a shy teenager gains spider-like abilities that he uses to fight injustice as a masked superhero and face a vengeful enemy",
     },
     {
       title: "Outsider",
       img: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-mistery-movie-poster-design-template-2ec690d65c22aa12e437d765dbf7e4af_screen.jpg?ts=1680854635",
+      duration: "2h 1m",
+      date: "11-11-24",
+      description:
+        "After being bitten by a genetically-modified spider, a shy teenager gains spider-like abilities that he uses to fight injustice as a masked superhero and face a vengeful enemy",
     },
   ]);
 
@@ -36,30 +56,48 @@ export default function Body() {
         <Input
           placeholder="Search video"
           startContent={<FaVideo />}
-          className="flex max-w-xs"
+          className="flex md:max-w-xs "
         />
       </div>
-      <div className="grid md:grid-cols-4 p-4 sm:grid-cols-1">
-        {poster.map((post, index) => (
-          <div className="flex w-64 p-4" key={index}>
-            <Card>
-              <CardBody>
-                <Image
-                  alt={post.title}
-                  className="object-cover rounded-xl"
-                  src={post.img}
-                  width={270}
-                />
-                <p className="flex justify-center">{post.title}</p>
-              </CardBody>
-              <Divider />
-              <CardFooter className="flex justify-between">
-                <Button color="primary">Approve</Button>
-                <Button color="danger">Delete</Button>
-              </CardFooter>
-            </Card>
-          </div>
-        ))}
+
+      <div className=" p-4 lg:p-0 w-full  overflow-y-scroll h-[calc(100vh-74px)] ">
+        <div className="flex flex-col items-stretch w-full max-w-screen-xl mx-auto gap-4 py-4">
+          {poster.map((post, index) => (
+            <div className="md:flex-1 w-full " key={index}>
+              <Card className="flex ">
+                <CardBody>
+                  <div className="md:flex-row gap-8 flex flex-col items-center lg:items-start">
+                    <Image
+                      alt={post.title}
+                      width={200}
+                      height={250}
+                      className="object-cover rounded-xl "
+                      src={post.img}
+                    />
+                    <div className="flex flex-col">
+                      <div className="flex w-full justify-between">
+                        <p className="flex justify-center font-bold">
+                          {post.title}
+                        </p>
+                        <p className="flex justify-center">{post.date}</p>
+                      </div>
+                      <div className="flex flex-col gap-8">
+                        <p className="flex font-light">{post.duration}</p>
+                        <p className="flex  justify-center">
+                          {post.description}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </CardBody>
+                <CardFooter className="flex justify-end gap-4">
+                  <Button color="primary">Approve</Button>
+                  <Button color="danger">Delete</Button>
+                </CardFooter>
+              </Card>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
